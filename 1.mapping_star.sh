ad770ff495671df9f71782fdcbb66fde0435c6ff
wzuhou <wuz1992@hotmail.com>
2022-03-22 16:35:46 +0000

    Update 1.mapping_star.sh

--- 1.mapping_star.sh
+++ 1.mapping_star.sh
@@ -8,13 +8,32 @@
 #$ -P roslin_smith_grp
 
 . /etc/profile.d/modules.sh
-#To run this code
-#for prefix in `cat name.list25`;do sh ZW.examp1.all25tot2.sh $prefix; done
-
 module load roslin/star/2.5.3a
+#################################################################
+#Prepare the STAR index first: GRCg6a
+#Usage sh 1.mapping_star.sh
+
+#Input1
+ref='<path_to_file>/GCF_ABC_genomic.fna'
+anno='<path_to_file>/GCF_ABC_genomic.gtf'
+
+#1. basic options to generate genome indices are as follows:
+STAR \
+--runThreadN 10 \
+--runMode genomeGenerate \
+--genomeDir ./GRCg6a \
+--genomeSAindexNbases 10 \
+--genomeFastaFiles ${ref} \
+--sjdbGTFfile ${anno} \
+--sjdbOverhang 149
+
+##################################################################
+#Mapping usage
+#for prefix in `cat name.list25`;do sh 1.mapping_star.sh $prefix; done
+
 prefix=$1
 
-#Input
+#Input2
 listR1=<path to Fastq>/${prefix}/${prefix}_R1.fastq.gz 
 listR2=<path to Fastq>/${prefix}/${prefix}_R2.fastq.gz
 output=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/mapping/${prefix}
@@ -23,8 +42,7 @@ ls  ${listR1} ${listR2}
 echo ${output}
 mkdir -p /exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/mapping
 
-#Prepare the STAR index first: GRCg6a
-
+#2. Mapping reads to reference 
 STAR --genomeDir GRCg6a \
 --runThreadN 10 \
 --readFilesCommand zcat \
