02ed4dc64882ea27d3e8028442866a33b97669aa
wzuhou <wuz1992@hotmail.com>
2022-04-07 14:22:13 +0100

    Create 1.multiFQ_make_mapping_star.sh

---
+++ 1.multiFQ_make_mapping_star.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#make sample list
+for prefix in `less LALO_RNA_list.txt`; do grep $prefix Fastq_list_1|cut -f 2 >${prefix}_list;done
+
+#make mulFQ file for mapping
+for prefix in `less LALO_RNA_list.txt`;do \
+#SubDir=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/RNA_LALO/mhindle/LALO_RNASeq_newgenome/Masked_mapping/Rawdata/$prefix
+#To make ${prefix}_mulFQ
+#READS 1
+for i in `less ${prefix}_list`; do \
+printf  "${prefix}/paired_${i}1.fastq.gz,"
+done >>${prefix}_mulFQ
+#space
+printf " ">>${prefix}_mulFQ
+#READS 2
+for j in `less ${prefix}_list`; do \
+printf  "${prefix}/paired_${i}2.fastq.gz,"
+done >>${prefix}_mulFQ
+sed -i 's/\, / /' ${prefix}_mulFQ
+sed -i 's/\,$/ /' ${prefix}_mulFQ
+done
