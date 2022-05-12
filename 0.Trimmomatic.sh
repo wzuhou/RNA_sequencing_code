68e9d426d7c30bf2a373d6b3ceeff9a5dc1e3abb
wzuhou <wuz1992@hotmail.com>
2022-05-12 11:32:35 +0100

    Update 0.Trimmomatic.sh

--- 0.Trimmomatic.sh
+++ 0.Trimmomatic.sh
@@ -18,6 +18,7 @@ module load igmm/apps/trimmomatic/0.39
 #module load java
 
 #cp /exports/igmm/software/pkg/el7/apps/trimmomatic/0.36/adapters/TruSeq3-PE-2.fa ./
+dir=<file_dir>
 input1=<read1.fastq.gz> #$1
 input2=<read2.fastq.gz> #$2
 
@@ -28,5 +29,9 @@ echo "inputfile Forward R1: $input1 and Reverse R2 $input2"
 
 #Create folders for Trimmed reads
 mkdir -p Trim
+if [ ! -f TruSeq3-PE.fa ];then
+cp /exports/igmm/software/pkg/el7/apps/trimmomatic/0.36/adapters/*.fa ./
+fi
+ 
 #Run Trimmomatic
-trimmomatic PE -threads 6 -phred33 $input1 $input2 Trim/paired_$input1 Trim/unpaired_$input1 Trim/paired_$input2 Trim/unpaired_$input2 ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+trimmomatic PE -threads 6 -phred33 ${dir}/$input1 ${dir}/$input2 Trim/paired_$input1 Trim/unpaired_$input1 Trim/paired_$input2 Trim/unpaired_$input2 ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
