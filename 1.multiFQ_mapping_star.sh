4ba2b287870a7c99f8a7e81590d7ce4d4d848439
wzuhou <wuz1992@hotmail.com>
2022-05-18 13:42:01 +0100

    Update 1.multiFQ_mapping_star.sh

--- 1.multiFQ_mapping_star.sh
+++ 1.multiFQ_mapping_star.sh
@@ -14,7 +14,6 @@
 #$ -l h_vmem=8G
 #$ -M zwu33@ed.ac.uk
 #$ -m baes
-#$ -P roslin_smith_grp
 #$ -pe sharedmem 4
 
 # If you plan to load any software modules, then you must first initialise the modules framework.
@@ -30,10 +29,18 @@ module load igmm/apps/STAR/2.7.8a
 
 #STAR mapping
 mkdir -p mapping
+
+##Example LALO_RNA_list.txt
+##head -n4  LALO_RNA_list.txt
+##189_Gonad
+##189_Heart
+##189_Hypothalamus
+##189_Liver
+#######################################
+# If you have Multiple fastq pairs (i.e. > 2 fastq input)
 for prefix in `less LALO_RNA_list.txt`;do \
 
-#prefix=$1 #Sample_list.txt e.g. K1_1 $i
-
+#prefix=$1 #Sample_list.txt e.g. 189_Gonad $i
 #listR1=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/Pipeline_RNA/data/K_data/raw_data/${prefix}/${prefix}_1.fq.gz
 #listR2=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/Pipeline_RNA/data/K_data/raw_data/${prefix}/${prefix}_2.fq.gz
 output=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/RNA_LALO/mhindle/LALO_RNASeq_newgenome/Masked_mapping/mapping/${prefix}
@@ -49,3 +56,20 @@ $mytext \
 --outFilterType BySJout --outSAMunmapped None --outReadsUnmapped Fastx --outFileNamePrefix ${output} --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 7900000000"
 
 done
+#########################################
+# If you have Single fastq pair (i.e. ==2 fastq input)
+#STAR mapping
+prefix=189_Gonad #Sample_list.txt e.g. 189_Gonad or $1
+
+listR1=<path_to_file>/<read1.fastq.gz> #$1
+listR2=<path_to_file>/<read2.fastq.gz> #$2
+output=<path_to_outputfile>/mapping/${prefix}
+ref=<path_to_file>/Reference_files
+
+ls  ${listR1} ${listR2}
+echo ${output}
+#Prepare the STAR index first: ref
+
+STAR --genomeDir ${ref} --runThreadN 4 --readFilesCommand zcat --readFilesIn ${listR1} ${listR2} --outFilterType BySJout --outSAMunmapped None --outReadsUnmapped Fastx --outFileNamePrefix ${output} --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 7900000000
+
+####################END###############################
