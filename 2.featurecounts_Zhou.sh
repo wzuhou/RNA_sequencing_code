e06f65ee640a4de0dea366ece1d4df549985a532
wzuhou <wuz1992@hotmail.com>
2022-05-23 09:01:11 +0100

    Update 2.featurecounts_Zhou.sh

--- 2.featurecounts_Zhou.sh
+++ 2.featurecounts_Zhou.sh
@@ -7,13 +7,16 @@
 #$ -M zwu33@ed.ac.uk
 #$ -m baes
 
-
+. /etc/profile.d/modules.sh
 date
 featurecounts=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/Install/subread-2.0.0-Linux-x86_64/bin/featureCounts
+#OR
+module load roslin/subread/1.5.2
+
 # One sample
 # sample=$1
 # $featurecounts -p -C -t exon -g gene_id -T 12 -a star_index/Gallus_gallus.GRCg6a.95.gtf -o F_quantification/${sample} mapping/${sample}.Aligned.sortedByCoord.out.newheader.RG.bam >& F_quantification/$sample.featurecounts_sortedByCoord.log
 
 #In batch
-$featurecounts -p -C -t exon -g gene_id -T 12 -a star_index/Gallus_gallus.GRCg6a.95.gtf -o /exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/Code_shared/Output_samples /exports/cmvm/eddie/eb/groups/smith_grp/Zhou_Wu/mapping/*.Aligned.sortedByCoord.out.bam >& /exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/Code_shared/Output_samples.featurecounts_sortedByCoord.log
+featureCounts -p -C -t exon -g gene_id -T 12 -a star_index/Gallus_gallus.GRCg6a.95.gtf -o /exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/Code_shared/Output_samples /exports/cmvm/eddie/eb/groups/smith_grp/Zhou_Wu/mapping/*.Aligned.sortedByCoord.out.bam >& /exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/Code_shared/Output_samples.featurecounts_sortedByCoord.log
 
