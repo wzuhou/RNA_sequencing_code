c2bf4614a7f3e822563f2b40af3b4b3c827d280a
wzuhou <wuz1992@hotmail.com>
2022-07-13 10:36:23 +0100

    Update 1.multiFQ_pre_mapping_star.sh

--- 1.multiFQ_pre_mapping_star.sh
+++ 1.multiFQ_pre_mapping_star.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
+#make Fastq_list_1
+#ls */*.fq.gz >Fastq_list
+ls */*1.fq.gz>Fastq_list_1
+sed -i 's/\//\t/g' Fastq_list_1
+sed -i 's/1\.fq\.gz/ /g' Fastq_list_1
+
 #make sample list
-for prefix in `less LALO_RNA_list.txt`; do grep $prefix Fastq_list_1|cut -f 2 >${prefix}_list;done
-################################
-#This code is optional when you have multiple fastqc files for one individual
+for prefix in `less ../Sample_RNA2.txt`; do grep $prefix Fastq_list_1|cut -f 2 >${prefix}_list;done
 
 #make mulFQ file for mapping
-for prefix in `less LALO_RNA_list.txt`;do \
-#SubDir=/exports/cmvm/eddie/eb/groups/smith_grp/Zhou_wu/RNA_LALO/mhindle/LALO_RNASeq_newgenome/Masked_mapping/Rawdata/$prefix
+for prefix in `less ../Sample_RNA2.txt`;do \
 #To make ${prefix}_mulFQ
 #READS 1
 for i in `less ${prefix}_list`; do \
-printf  "${prefix}/paired_${i}1.fastq.gz,"
+printf  "${prefix}/${i}1.fq.gz,"
 done >>${prefix}_mulFQ
 #space
-printf " ">>${prefix}_mulFQ
+printf " ">> ${prefix}_mulFQ
 #READS 2
 for j in `less ${prefix}_list`; do \
-printf  "${prefix}/paired_${j}2.fastq.gz,"
+printf  "${prefix}/${j}2.fq.gz,"
 done >>${prefix}_mulFQ
 sed -i 's/\, / /' ${prefix}_mulFQ
 sed -i 's/\,$/ /' ${prefix}_mulFQ
 done
+
+#If error, remember to 
+#rm *_mulFQ
